7dc7a8c1e8c1801ab82d26b889c810c5a2b6cf53
santicasa <106031549+santicasa@users.noreply.github.com>
2022-05-27 21:01:00 -0300

    Update lectura_y_guardado.c
    Tuve mucho quilombo para modificar este archivo en github simplemente borré todo y copié del que estaba usando yo en mi computadora. Espero no haber dado problemas adicionales.

--- lectura_y_guardado.c
+++ lectura_y_guardado.c
@@ -10,14 +10,57 @@
  * que luego se pasará a la función readfile.
  **/
 
+/**
+ * Ahora este archivo también crea un arreglo de enteros con el "peso" de cada
+ * caracter. Más detalles en la función peso_arreglo_caracteres.
+ */
+
+/**
+ * arreglo_con_peso es solamente un arreglo de 256 enteros, el número de
+ * caracteres ASCII que existen.
+ * nota: verificar si esta función debería estar dentro del main
+ */
+
+int * arreglo_con_peso() {
+  int * arreglo = malloc(sizeof(int)*256);
+  for (int i = 0; i < 256; i++) {
+    arreglo[i] = 0;
+  }
+  return arreglo;
+}
+
+/**
+ * peso_arreglo_caracteres lee todo el arreglo de carcateres que contiene el
+ * texto y guarda cuantas apariciones hay de cada uno en un arreglo de enteros
+ * donde cada indice de este arreglo es un caracter ASCII.
+ */
+
+int * peso_arreglo_caracteres(int *arreglo, char *buff, int len) {
+  char c;
+  for (int i = 0; i < len; i++) {
+    c = buff[i];
+    arreglo[(unsigned)c] += 1;
+  }
+  return arreglo;
+}
+
 int main() {
   const char* path = "prueba.txt";
+  int len; 
   char* buff;
-  int* len;
 
-  buff = readfile(path, len);
-  printf("%s\n", buff);
-  printf("%d\n", *len);
+  int *arreglo = arreglo_con_peso();
+
+  buff = readfile(path, &len);
+
+  peso_arreglo_caracteres(arreglo, buff, len);
+
+  printf("buff = %s\n", buff);
+  //~ detalle, este arreglo de caracteres parece imprimir basura al final
+  printf("largo = %d\n", len);
+  printf("peso 'e' = %d\n", arreglo[101]);
+
+  free(arreglo);
 
   return 0;
 }
