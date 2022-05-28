0ea2d978cbe43f69dde2cf37ed249a11dade5139
Jordis2311 <50746582+Jordis2311@users.noreply.github.com>
2022-05-28 19:11:19 -0300

    Update Guia.c

--- Guia.c
+++ Guia.c
@@ -22,3 +22,14 @@ En donde peso es cada aparicion de la letra
 4 - Guardar todas las Hojas en una Lista enlazada
 
 5 - Recorrer la lista agarrando los arboles de minimo peso y uniendolos, sumando los pesos y uniendolos arboles, volviendolos a insertar a la lista hasta que solo quede 1 Arbol
+
+6 - Recorrer el arbol y guardar el recorrido hasta llegar a cada hoja guardando en un array de strings (char**) utilizando la letra que esta en la hoja como indice, el recorrido
+
+7 - Recorrer y guardar en un texto la estructura del arbol (1 es rama 0 es hoja) para asi ponerlo en un archivo de texto
+
+8 - Recorrer el texto original y cada caracter traducirlo al codigo correspondiente para asi y colocarlo este texto traducido en otro array char*
+
+9 - Utilizar implode para el texto codificado y guardarlo en un archivo con writefile (llamado f.txt.hf siendo f el nombre del archivo original)
+
+10 - guardar la estructura del arbol y las posiciones de las hojas en otro archivo (llamado f.txt.tree)
+
