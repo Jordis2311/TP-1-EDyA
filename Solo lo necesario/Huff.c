cf0288cc8977c0b223c829b3a12f6e5822970ddc
santicasa <106031549+santicasa@users.noreply.github.com>
2022-06-02 15:29:31 -0300

    Update Huff.c

--- Solo lo necesario/Huff.c
+++ Solo lo necesario/Huff.c
@@ -16,18 +16,18 @@
  * Compresion de Huffman
  */
 
-int main( int argc, char **argv){
+int main(int argc, char **argv) {
   assert(argc > 1);
-  assert((strcmp(argv[1],"C") == 0) || (strcmp(argv[1],"D") == 0));
+  assert((strcmp(argv[1], "C") == 0) || (strcmp(argv[1], "D") == 0));
 
-  if(strcmp(argv[1],"C") == 0){
+  if (strcmp(argv[1], "C") == 0) {
     assert(argc == 3);
 
     int len;
     // Leemos el archivo y lo guardamos junto con su longitud
     // char* buff: contenido del archivo a comprimir
     // int len: longitud del archivo
-    char* buff = readfile(argv[2],&len);
+    char *buff = readfile(argv[2], &len);
 
     // Creamos la lista que contiene a las apariciones de cada caracter
     // int *pesos: array que contiene las repeticiones de los caracteres
@@ -46,101 +46,101 @@ int main( int argc, char **argv){
     // los Datos de cada lista, liberando el espacio ocupado por esta
     BSTree arbol_huffman = combinar_lista(lista_arb);
 
-    // char** codigos: array de strings que contendran los codigos de
+    // char **codigos: array de strings que contendran los codigos de
     // cada caracter ordenados segun el ASCII de dicho caracter
     char buffer_codigos[30];
-    char **codigos = malloc(sizeof(char*)*256);
+    char **codigos = malloc(sizeof(char*) * 256);
 
-    // char* estructura_arbol: array de 1 y 0 que representa
-    // como es el arbol (1 = hoja,0 = rama)
-    char *estructura_arbol =malloc(sizeof(char)*512);
-    // char* orden_letras: array que contiene a las caracteres segun
+    // char *estructura_arbol: array de 1 y 0 que representa
+    // como es el arbol (1 = hoja, 0 = rama)
+    char *estructura_arbol = malloc(sizeof(char) * 512);
+
+    // char *orden_letras: array que contiene a las caracteres segun
     // aparecen en el arbol recorriendolo inorder
-    char *orden_letras = malloc(sizeof(char)*257);
-    int pos_est = 0,pos_letra = 0;
+    char *orden_letras = malloc(sizeof(char) * 257);
+    int pos_est = 0, pos_letra = 0;
 
     // Definimos los codigos de cada caracter utilizando el arbol
     // de Huffman guardandolos en codigos
     // tambien guarda la estructura del arbol en estructura_arbol
     // y los caracteres en el orden que aparecen
-    codigos_arbol(arbol_huffman,buffer_codigos,0,codigos
-                  ,estructura_arbol,&pos_est,orden_letras,&pos_letra);
+    codigos_arbol(arbol_huffman, buffer_codigos, 0, codigos,
+                  estructura_arbol, &pos_est, orden_letras, &pos_letra);
 
     estructura_arbol[pos_est] = '\0';
 
     orden_letras[pos_letra] = '\0';
 
-    // char* tree: es el texto que contendra el archivo .tree
+    // char *tree: es el texto que contendra el archivo .tree
     // la concatenacion de estructura_arbol y orden_letras
-    char *tree = unir_arb_let(estructura_arbol,511,orden_letras,256);
+    char *tree = unir_arb_let(estructura_arbol, 511, orden_letras, 256);
 
     // int longitud_bits: es la longitud que tendra la cadena de bits
     // que representa al texto original antes del implode
-    int longitud_bits = longitud_Archivo_final(pesos,codigos,256);
+    int longitud_bits = longitud_archivo_final(pesos, codigos, 256);
 
-    // char* traduccion: es el archivo original cuyos caracteres fueron
+    // char *traduccion: es el archivo original cuyos caracteres fueron
     // cambiados por sus codigos correspondientes
-    char *traduccion = traducir(buff,len,codigos,longitud_bits);
+    char *traduccion = traducir(buff, len, codigos, longitud_bits);
 
     int hlen;
-    // char* huffman: es el texto que ira dentro del archivo .hf
+    // char *huffman: es el texto que ira dentro del archivo .hf
     // es decir la traduccion luego de convertirse en caracteres
-    char *huffman = implode(traduccion,longitud_bits,&hlen);
+    char *huffman = implode(traduccion, longitud_bits, &hlen);
 
-    // char* n_h y n_t fueron creados para modificar el nombre
+    // char *n_h y n_t fueron creados para modificar el nombre
     // del archivo original (agregandole .hf y .tree)
-    char* n_h = strcat(argv[2],".hf");
+    char *n_h = strcat(argv[2], ".hf");
 
     // Escribimos en "nombre_archivo.hf" la compresion realizada
-    writefile(n_h,huffman,hlen);
-    argv[2][strlen(argv[2])-3] = '\0';
+    writefile(n_h, huffman, hlen);
+    argv[2][strlen(argv[2]) - 3] = '\0';
 
-    char* n_t = strcat(argv[2],".tree");
+    char *n_t = strcat(argv[2], ".tree");
     // Escribimos en "nombre_archivo.tree" la estructura del arbol de Huffman
     // que utilizamos para comprimirlo
-    writefile(n_t,tree,767);
+    writefile(n_t, tree, 767);
 
     free(tree);
     free(huffman);
     free(traduccion);
-    for(int i=0;i<256;i++) free(codigos[i]);
+    for (int i = 0; i < 256; i++) free(codigos[i]);
     free(codigos);
     free(orden_letras);
     free(estructura_arbol);
     liberar_arbol(arbol_huffman);
     free(pesos);
     free(buff);
-  } 
-  else{
+  } else {
     assert(argc == 4);
 
-    int alen,hlen,blen;
+    int alen, hlen, blen;
     int pos_arb = 0, pos_let = 511;
-    // char* arbol_txt: es la codificacion en texto del arbol
+    // char *arbol_txt: es la codificacion en texto del arbol
     // que utilizamos para comprimir el archivo
-    char *arbol_txt = readfile(argv[3],&alen);
+    char *arbol_txt = readfile(argv[3], &alen);
 
     // BSTree arbol: es el arbol de huffman que se utilizo 
     // para comprimir el texto
-    BSTree arbol = texto_a_arbol(arbol_txt,&pos_arb,&pos_let);
+    BSTree arbol = texto_a_arbol(arbol_txt, &pos_arb, &pos_let);
 
-    // char* txt_cod: es el texto implosionado de la traduccion
+    // char *txt_cod: es el texto implosionado de la traduccion
     // a bits del archivo original
-    char *txt_cod = readfile(argv[2],&hlen);
+    char *txt_cod = readfile(argv[2], &hlen);
     
-    // char* txt_bits: es el binario del archivo comprimido
-    char *txt_bits = explode(txt_cod,hlen,&blen);
+    // char *txt_bits: es el binario del archivo comprimido
+    char *txt_bits = explode(txt_cod, hlen, &blen);
 
     int tlen;
     // char* texto_original: es (valga la redundancia) el texto
     // del archivo original
-    char* texto_original = decodificar(txt_bits,blen,arbol,&tlen);
+    char* texto_original = decodificar(txt_bits, blen, arbol, &tlen);
 
-    argv[2][strlen(argv[2])-3] = '\0';
-    char* nombre_final = strcat(argv[2],".dec");
+    argv[2][strlen(argv[2]) - 3] = '\0';
+    char* nombre_final = strcat(argv[2], ".dec");
     
     // escribimos devuelta el archivo descomprimido en "nombre_archivo.dec"
-    writefile(nombre_final,texto_original,tlen);
+    writefile(nombre_final, texto_original, tlen);
 
     liberar_arbol(arbol);
     free(texto_original);
